83966ca06d80b7e11fa57a1e25617098f37f1010
Honório Gomes <hgunicamp@gmail.com>
2015-02-14 23:07:15 -0200

    Revert "Inserting makeCacheMAtrix() and cacheSolve functions"
    This reverts commit f72e68a99e32f666305073cd0894a39bb5748270.

--- cachematrix.R
+++ cachematrix.R
@@ -4,30 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-	inverse <- NULL
-	Set     <- function(y) {
-		x       <<- y
-		inverse <<- NULL
-	}
-	Get        <- function()    x
-	SetInverse <- function(inv) inverse <<- inv
-	GetInverse <- function()    inverse
-	list("Get"        = Get,
-	     "Set"        = Set,
-	     "GetInverse" = GetInverse,
-	     "SetInverse" = SetInverse )
+
 }
+
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-	inverse <- x$GetInverse()
-	if (!is.null(inverse)) {
-		message("From Cache")
-		return(inverse)
-	}
-	data <- x$Get()
-	inverse <- solve(data)
-	x$SetInverse(inverse)
-	inverse
+        ## Return a matrix that is the inverse of 'x'
 }
