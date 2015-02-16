1b352f8e2d46bb88438cb897e360441be8c003cb
Honório Gomes <hgunicamp@gmail.com>
2015-02-16 14:30:44 -0200

    Inserting comments for functions makeCacheMatrix and cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix(x): function
+## - Arg: 
+##     x: matrix()
+## - Use:
+##     aMatrix <- makeCacheMatri(x)
+## - Description:
+##     Returns a list of functions to manipulate a square matrix internally
+##     stored, and its inverse, created using the "x" argument.
+##     The functions are:
+##     - Get(): Returns the internally stored square matrix. 
+##         - Use:
+##             y <- aMatrix$Get()
+##     - Set(x): Stores internally the matrix passed through
+##             argument "x". It invalidates the value of the
+##             inverse previously stored.
+##         - Args:
+##             x: matrix()
+##         - Use:
+##             aMatrix$Set(x)
+##     - GetInverse(): Returns the value of the inverse matrix stored
+##                   internally. If it was not calculated yet, resturns NULL.
+##         - Use:
+##             y <- aMatrix$GetInverse()
+##     - SetInverse(x): Stores internally the matrix passed through
+##                    argument "x" as the inverse of the previous stored
+##                    matrix.
+##         - Args:
+##             x: A square matrix
+##         - Use:
+##             aMatrix$SetInverse(x)
 
 makeCacheMatrix <- function(x = matrix()) {
 	inverse <- NULL
@@ -18,16 +44,26 @@ makeCacheMatrix <- function(x = matrix()) {
 	     "SetInverse" = SetInverse )
 }
 
-## Write a short comment describing this function
+## cacheSolve(x,...): function
+## - Arg: 
+##     x: makeCacheMatrix()
+## - Use:
+##     y <- cacheSolve(x,...)
+## - Description:
+##     Returns an internally stored inverse matrix stored into a list created
+##     through makeCacheMatrix function. If the inverse was not calculated yet,
+##     calculates its inverse, updates the list and returns the inverse
+##     calculed.
+
 
 cacheSolve <- function(x, ...) {
 	inverse <- x$GetInverse()
 	if (!is.null(inverse)) {
-		message("From Cache")
+		# From Cache
 		return(inverse)
 	}
 	data <- x$Get()
-	inverse <- solve(data)
+	inverse <- solve(data,...)
 	x$SetInverse(inverse)
 	inverse
 }
